2b301e2ef7a6b2ffb6db9990c4c65a4e3a34ef4f
eric santos <eric355402@yahoo.com>
2021-09-20 19:44:44 -0600

    created api services fetching coins route with redux

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "385c57f4d8msh733adc363b22fdbp1bd332jsn70091e858746",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+//useGetCryptoQuery is provided by redux, referencing the getCryptos above coming from cryptoApi also above
+
+export const { useGetCryptosQuery } = cryptoApi;
